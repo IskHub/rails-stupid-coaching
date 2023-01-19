b2c42f1b9e0ef1fc98d40e3cbac479c3668acaef
Iskander Hughes <iskanderhughes@hotmail.com>
2023-01-19 18:39:48 +0000

    logic is there

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,11 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+def ask
+end
 
   def answer
+    @questions
+    if params[:question] == "hello"
+      @coach = "I don't care, get dressed and go to work!"
+    end
   end
 end
